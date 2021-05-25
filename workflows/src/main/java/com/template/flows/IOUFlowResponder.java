3a6f4c1802e1da6aed1a5ffcaa9168164d8454ae
mebinjoe <mebinjoe@gmail.com>
2021-05-25 18:17:00 +0530

    Template workflows

---
+++ workflows/src/main/java/com/template/flows/IOUFlowResponder.java
@@ -0,0 +1,24 @@
+package com.template.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import net.corda.core.flows.*;
+import net.corda.core.transactions.SignedTransaction;
+
+// ******************
+// * Responder flow *
+// ******************
+@InitiatedBy(IOUFlow.class)
+public class IOUFlowResponder extends FlowLogic<Void> {
+    private final FlowSession otherPartySession;
+
+    public IOUFlowResponder(FlowSession otherPartySession) {
+        this.otherPartySession = otherPartySession;
+    }
+
+    @Suspendable
+    @Override
+    public Void call() throws FlowException {
+        subFlow(new ReceiveFinalityFlow(otherPartySession));
+        return null;
+    }
+}
