9f576ee5e3eb7e1c8a058cae4840e19e59b5d7c9
mebinjoe <mebinjoe@gmail.com>
2021-05-25 18:16:38 +0530

    Template contracts

---
+++ contracts/src/main/java/com/template/contracts/TemplateContract.java
@@ -0,0 +1,30 @@
+package com.template.contracts;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
+import static net.corda.core.contracts.ContractsDSL.requireThat;
+import java.util.List;
+
+// ************
+// * Contract *
+// ************
+public class TemplateContract implements Contract {
+    // This is used to identify our contract when building a transaction.
+    public static final String ID = "com.template.contracts.TemplateContract";
+
+    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
+    // does not throw an exception.
+    @Override
+    public void verify(LedgerTransaction tx) {
+    }
+
+    // Used to indicate the transaction's intent.
+    public interface Commands extends CommandData {
+        //In our hello-world app, We will only have one command.
+        class Send implements Commands {}
+    }
+}
